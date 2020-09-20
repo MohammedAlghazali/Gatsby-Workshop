8521d878d0c2d39c74d6857c4682fd9c87ff925e
mohammedAlghazali <hamood-monzer11@hotmail.com>
2020-09-21 01:30:11 +0300

    impelent the workshop code

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Container from "../components/container"
+
+export default function About() {
+  return (
+    <Container>
+      <h1>About CSS Modules</h1>
+      <h1 className="another_header">About CSS Modules</h1>
+      <p>CSS Modules are cool</p>
+    </Container>
+  )
+}
